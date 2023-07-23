f0aeeeaf72974dd02908612198a742c52e98d99b
vrylkov05@gmail.com <hskmrjd05>
2023-07-23 19:12:15 +0300

    SingleTon

--- src/Zoo/Zoo.java
+++ src/Zoo/Zoo.java
@@ -7,10 +7,39 @@ import java.util.List;
 
 public class Zoo {
     private List<Animal> animals = new ArrayList<>();
+    private static Zoo instatnce;
 
-    public String addNewAnimal(Animal newAnimal){
+    private Zoo() {
+
+    }
+
+    public static Zoo getInstance() {
+        if (instatnce == null) {
+            synchronized (Zoo.class) {
+                if (instatnce == null) {
+                    instatnce = new Zoo();
+                }
+            }
+        }
+        return instatnce;
+    }
+
+    public String addNewAnimal(Animal newAnimal) {
         animals.add(newAnimal);
         return "добавлено";
     }
 
+    public String removeAnimal(Animal animal) {
+        for (Animal anim : animals) {
+            if (anim.equals(animal)) {
+                animals.remove(anim);
+                return "удалено";
+            }
+        }
+        return "";
+    }
+
+    public int getCountAnimal() {
+        return animals.size();
+    }
 }
